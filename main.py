d18723d3fca85c24c365018dac55cf4f95acdfc1
lokendar jangid <lokendarjangid@gmail.com>
2023-02-19 22:17:50 +0530

    new update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import cv2
 import numpy as np
 import HandTrackingModule as htm
 import time
 import pyautogui as pg
-from ctypes import cast, POINTER   # pip install pycaw
+from ctypes import cast, POINTER  # pip install pycaw
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
@@ -29,10 +30,9 @@ interface = devices.Activate(
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
-volRange = volume.GetVolumeRange() # volume range (-65.25, 0.0)
+volRange = volume.GetVolumeRange()  # volume range (-65.25, 0.0)
 minVol = volRange[0]
 maxVol = volRange[1]
-
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -52,10 +52,11 @@ while True:
 
     #  Get the tip of the index and middle fingers
     if len(lmList) != 0:
+        Tx, Ty = lmList[4][1:]
         Ix, Iy = lmList[8][1:]
         Mx, My = lmList[12][1:]
-        Tx, Ty = lmList[4][1:]
         Rx, Ry = lmList[16][1:]
+        Px, Py = lmList[20][1:]
         # print(Ix,Iy,Mx,My)
 
         #  Check which fingers are up
@@ -64,8 +65,7 @@ while True:
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
 
         #  Only Index Finger : Moving Mode
-        if fingers[1] == 1 and fingers[2] == 0 and fingers[0]==0 and fingers[3] == 0:
-
+        if fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
             #  Convert Coordinates
             x3 = np.interp(Ix, (frameR, wCam - frameR), (0, wScr))
             y3 = np.interp(Iy, (frameR, hCam - frameR), (0, hScr))
@@ -80,7 +80,7 @@ while True:
             plocX, plocY = clocX, clocY
 
         #  Both Index and middle fingers are up : Clicking Mode
-        if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0 and fingers[3] == 0:
+        if fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0:
             #  Find distance between index finger
             lengthIM, img, lineInfo = detector.findDistance(8, 12, img)
             # print(lengthIM)
@@ -89,25 +89,39 @@ while True:
             if lengthIM < 40:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 pg.click()
-        
-        # Both Thumb and index figer are up : Volume mode
-        if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0:
+                
+
+        # Both Thumb and index finger are up : Volume mode
+        if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
             # Find distance between finger and thumb
             lengthTI, img, lineInfo = detector.findDistance(4, 8, img)
             # print(length)
             # Hand Range 50 to 300
             # volume Range -65 to 0
-            vol = np.interp(lengthTI,[50,250],[minVol,maxVol])
-            volBar = np.interp(lengthTI,[50,250],[400,150])
+            vol = np.interp(lengthTI, [50, 250], [minVol, maxVol])
+            volBar = np.interp(lengthTI, [50, 250], [400, 150])
             # print(int(length),vol)
             volume.SetMasterVolumeLevel(vol, None)
-            cv2.rectangle(img,(50,150),(85,400),(255, 0, 0),3)
-            cv2.rectangle(img,(50,int(volBar)),(85,400),(255, 0, 0),cv2.FILLED)
-
+            cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+            cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
 
             if lengthTI < 50:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
 
+        # Index, middle and ring finger are up : scroll
+        if fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 0:
+            lenghtIM, img, lineInfo = detector.findDistance(8, 12, img)
+            lengthMR, img, lineInfo = detector.findDistance(12, 16, img)
+
+            if lenghtIM < 40 and lengthMR < 45:
+                cv2.circle(img, (Mx, My), 15, (0, 255, 0), cv2.FILLED)
+                # print(My)
+                if My == 250:
+                    pass
+                elif My > 250:
+                    pg.scroll(20)
+                elif My < 250:
+                    pg.scroll(-20)
 
     #  Frame Rate
     ctime = time.time()
